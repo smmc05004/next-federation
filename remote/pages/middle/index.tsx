610da2285ff38e8bee62e269564cca3c4af93158
SeokhwaLee <seokhwa.lee@colorverse.co>
2023-05-18 17:29:44 +0900

    feat: react-query 테스트

--- remote/pages/middle/index.tsx
+++ remote/pages/middle/index.tsx
@@ -1,4 +1,16 @@
+import { useQuery } from "@tanstack/react-query"
+
+const fetchApi = async () => {
+  const result = await fetch('http://localhost:3001/api/hello')
+  console.log('result: ', result)
+
+  return result
+}
 const Page_Middle = () => {
+  const {data} = useQuery(['test'], () => fetchApi())
+  console.log('data: ', data)
+  
+
   return (
     <div>미들웨어 체크 페이지</div>
   )

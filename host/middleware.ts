a96ec16ec4a2409b67a52d6ffa9c6e77a9e25553
SeokhwaLee <seokhwa.lee@colorverse.co>
2023-05-19 17:09:51 +0900

    feat: react-query ssr 추가 (기능 작동은 x)

--- host/middleware.ts
+++ host/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  console.log('-----------------host middleware 실행-----------------------')
+  // console.log('-----------------host middleware 실행-----------------------')
   return;
 }
  
--- host/pages/middle.tsx
+++ host/pages/middle.tsx
@@ -1,5 +1,28 @@
-import MiddlePage from 'remote/middle'
+import { GetServerSidePropsContext } from 'next';
+import dynamic from 'next/dynamic';
+// import MiddlePage from 'remote/middle'
 
-const Middle = MiddlePage;
+//@ts-ignore
+const Middle = dynamic(import('remote/middle').then((mod) => mod.Page_Middle), {ssr: true});
 
-export default Middle
+const Page_Middle = () => {
+  return (
+    <div>
+      <Middle />
+    </div>
+  )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const page = await import('remote/middle');
+
+  if (page.getServerSideProps) {
+    return page.getServerSideProps(context);
+  }
+
+  return {
+    props: {},
+  };
+}
+
+export default Page_Middle
